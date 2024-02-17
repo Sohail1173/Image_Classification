32e09af6a1e2343098649085c8d152460b300cd9
Sohail1173 <mdsohailahmed711@gmail.com>
2024-02-17 15:35:15 +0530

    second commit

---
+++ plot_metrics.py
@@ -0,0 +1,29 @@
+import os
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+results_path =r'C:\Users\91808\Downloads\yolov8\results.csv'
+
+results = pd.read_csv(results_path)
+
+plt.figure()
+plt.plot(results['                  epoch'], results['             train/loss'], label='train loss')
+plt.plot(results['                  epoch'], results['               val/loss'], label='val loss', c='red')
+plt.grid()
+plt.title('Loss vs epochs')
+plt.ylabel('loss')
+plt.xlabel('epochs')
+plt.legend()
+
+plt.savefig("Loss vs epochs.png")
+
+
+plt.figure()
+plt.plot(results['                  epoch'], results['  metrics/accuracy_top1'] * 100)
+plt.grid()
+plt.title('Validation accuracy vs epochs')
+plt.ylabel('accuracy (%)')
+plt.xlabel('epochs')
+plt.savefig("Validation accuracy vs epochs.png")
+plt.show()
